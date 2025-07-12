d0bf85aa52858a6967f065a896038a6d33465905
primadita <pulomas10@hotmail.com>
2025-07-12 03:38:29 +0200

    add: fonts, img, header, link to css

--- script/db.js
+++ script/db.js
@@ -1,3 +1,39 @@
 class Menu {
+    // #region attributes
+    name;
+    category;
+    description;
+    img;
+    price;
+    // #endregion
+
+    constructor(dishName, dishCategory, dishDescription, dishImg, dishPrice){
+        this.name = dishName;
+        this.category = dishCategory;
+        this.description = dishDescription;
+        this.img = dishImg;
+        this.price = dishPrice;
+    }
+
+    // #region methods
+    // #endregion
+}
+
+class Restaurant {
+    // #region attributes
+    name;
+    address;
+    openingtime;
+    rate;
+    // #endregion
     
+    constructor(restName, restAddress, restOpeningTime, restRate){
+        this.name = restName;
+        this.address = restAddress;
+        this.openingtime = restOpeningTime;
+        this.rate = restRate;
+    }
+
+    // #region methods
+    // #endregion
 }
